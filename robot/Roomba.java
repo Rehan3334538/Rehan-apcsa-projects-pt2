2437591324726353e1a30e2a621ae67031da48fe
Rehan33345384 <rw9802@pleasantonusd.net>
2025-09-08 22:09:59 +0000

    working on loop to turn

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -4,64 +4,94 @@ import kareltherobot.*;
 
 public class Roomba implements Directions {
 
-	// Main method to make this self-contained
-	public static void main(String[] args) {
-		// LEAVE THIS ALONE!!!!!!
-		String worldName = "robot/basicRoom.wld";
+    // Main method to make this self-contained
+    public static void main(String[] args) {
+        // LEAVE THIS ALONE!!!!!!
+        String worldName = "robot/basicRoom.wld";
 
-		Roomba cleaner = new Roomba();
-		int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
-		System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
+        Roomba cleaner = new Roomba();
+        int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
+        System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
 
-	}
+    }
 
-	// declared here so it is visible in all the methods!
-	public Robot roomba;
+    // declared here so it is visible in all the methods!
+    public Robot roomba;
 
-	// You will need to add many variables!!
+    public int cleanRoom(String worldName, int startX, int startY) {
 
+        World.readWorld(worldName);
+        World.setVisible(true);
+        World.setDelay(5);
 
-	public int cleanRoom(String worldName, int startX, int startY) {
-
-		// A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
-        // Make sure it starts at startX and startY location.
-
-		World.readWorld(worldName);
-		World.setVisible(true);
-		Robot roomba = new Robot(9,9,South,9);
-		World.setDelay(5);
-
-
-		/** This section will have all the logic that takes the Robot to every location
-		 * and cleans up all piles of beepers. Think about ways you can break this
-		 * large, complex task into smaller, easier to solve problems.
-		 */
-roomba = new Robot( startX, startY, East, 0);
-		// the line below causes a null pointer exception
-		// what is that and why are we getting it?
-	int totalBeepersCleaned=0;
-		roomba.move();
-		roomba.move();
-		roomba.pickBeeper();
+        // Use instance variable instead of local declaration
+        roomba = new Robot(startX, startY, East, 0);
+        int totalBeepersCleaned = 0;
+      while (roomba.frontIsClear())
+{
 while (roomba.nextToABeeper())
 {
-	// body action
 	roomba.pickBeeper();
 }
-for (int i=1; i<=5; i++){
-			roomba.move();
-			roomba.turnLeft();
-			roomba.move();
-			roomba.turnLeft();
-			roomba.move();
-			}
-	}
+	roomba.move();
+}
+roomba.turnLeft();
+while (roomba.frontIsClear())
+{
+while (roomba.nextToABeeper())
+{
+	roomba.pickBeeper();
+}
+	roomba.move();
+}
+roomba.turnLeft();
+while (roomba.frontIsClear())
+{
+while (roomba.nextToABeeper())
+{
+	roomba.pickBeeper();
+}
+	roomba.move();
+}
+roomba.turnLeft();
+roomba.move();
+roomba.turnLeft();
+while (roomba.frontIsClear())
+{
+while (roomba.nextToABeeper())
+{
+	roomba.pickBeeper();
+}
+	roomba.move();
+}
+roomba.turnLeft();
+roomba.turnLeft();
+roomba.turnLeft();
+roomba.move();
+roomba.turnLeft();
+roomba.turnLeft();
+roomba.turnLeft();
+while (roomba.frontIsClear())
+{
+while (roomba.nextToABeeper())
+{
+	roomba.pickBeeper();
+}
+	roomba.move();
+}
+roomba.turnLeft();
+roomba.move();
+roomba.turnLeft();
+while (roomba.frontIsClear())
+{
+while (roomba.nextToABeeper())
+{
+	roomba.pickBeeper();
+}
+	roomba.move();
+}
 
-		
 
-
-		//int totalBeepersCleaned = 0; // Need to move this somewhere else.
-        // This method should return the total number of beepers cleaned up.
-		return totalBeeperCleaned;
-	}
+        return totalBeepersCleaned;
+    }
 }
