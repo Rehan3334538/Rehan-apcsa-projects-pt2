6e04da5badabc2353cd80120a65c6c1ae4b97a91
Rehan3334538 <rehanwilliam0@gmail.com>
2025-11-25 03:23:08 +0000

    card

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,19 @@
 package cards;
 
 public class Card {
-
-
+	private int suit;
+	private int value;
+	final private static String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+	final private static String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+	public Card(int suit, int value) {
+		this.suit = suit;
+		this.value = value;
+	}
+	public int getValue() {
+		return value;
+	}
+	@Override
+	public String toString() {
+		return values[value] + " of " + suits[suit];
+	}
 }
