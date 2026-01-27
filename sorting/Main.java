9a55fd26a3cee3aab758a6e4dc0e061cda54d82b
Rehan3334538 <rehanwilliam0@gmail.com>
2026-01-27 22:57:59 +0000

    random number array

--- sorting/Main.java
+++ sorting/Main.java
@@ -1,30 +1,25 @@
 package sorting;
 
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        // Task:
-        // 1. Implement missing sorting algorithms.
-        //    - InsertionSort.java
-        //    - SelectionSort.java
-        //
-        //  2. Add a method to class Main to generate a random array.
-        //  3. Provide random input to test suite and ensure your implementations
-        //     of InsertionSort and Selection sort continue to work.
-        //  4. Increase the loop count for TestSuite to get better performance results.
+    public static void main(String[] args) {
 
         int[] testInput = randomArray(5);
 
-        // Increase the loop count to get better results once it works.
-        // TestSuite.run(testInput, 10000);
+        // Increase this once everything works
         TestSuite.run(testInput, 1);
+        // TestSuite.run(testInput, 10000);
     }
 
-    public static int[] randomArray(int length)
-    {
-        // TODO: make this return an actual random array of the provided length.
-        int[] a = {10, 5, 3, 2};
-        return a;
-    }
+    public static int[] randomArray(int length) {
+        Random rand = new Random();
+        int[] arr = new int[length];
 
+        for (int i = 0; i < length; i++) {
+            arr[i] = rand.nextInt(100); 
+        }
+
+        return arr;
+    }
 }
