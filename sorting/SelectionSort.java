cb4055af6956e9ca5c46c0fd7f734b7f44bef5c6
Rehan3334538 <rehanwilliam0@gmail.com>
2026-01-29 22:27:57 +0000

    done selection sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -2,7 +2,29 @@ package sorting;
 
 public class SelectionSort implements Sorter {
 
+    @Override
     public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+        if (input == null || input.length <= 1) {
+            return;
+        }
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < input.length - 1; i++) {
+            
+            // Find the minimum element in unsorted part
+            int minIndex = i;
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element of unsorted part
+            if (minIndex != i) {
+                int temp = input[i];
+                input[i] = input[minIndex];
+                input[minIndex] = temp;
+            }
+        }
     }
 }
